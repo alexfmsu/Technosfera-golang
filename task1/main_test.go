364d7867609bc3fa48c57e1e6cedaa478351e487
alexfmsu <alexfmsu@mail.ru>
2017-06-16 07:45:40 +0300

    golang added

--- task1/main_test.go
+++ task1/main_test.go
@@ -10,33 +10,41 @@ func TestReturnInt(t *testing.T) {
 		t.Error("expected 1")
 	}
 }
+
 func TestReturnFloat(t *testing.T) {
 	if ReturnFloat() != float32(1.1) {
 		t.Error("expected 1.1")
 	}
 }
+
 func TestReturnIntArray(t *testing.T) {
 	if ReturnIntArray() != [3]int{1, 3, 4} {
 		t.Error("expected '[3]int{1, 3, 4}'")
 	}
 }
+
 func TestReturnIntSlice(t *testing.T) {
 	expected := []int{1, 2, 3}
 	result := ReturnIntSlice()
+
 	if !reflect.DeepEqual(result, expected) {
 		t.Error("expected", expected, "have", result)
 	}
 }
+
 func TestIntSliceToString(t *testing.T) {
 	expected := "1723100500"
 	result := IntSliceToString([]int{17, 23, 100500})
+
 	if expected != result {
 		t.Error("expected", expected, "have", result)
 	}
 }
+
 func TestMergeSlices(t *testing.T) {
 	expected := []int{1, 2, 3, 4, 5}
 	result := MergeSlices([]float32{1.1, 2.1, 3.1}, []int32{4, 5})
+
 	if !reflect.DeepEqual(result, expected) {
 		t.Error("expected", expected, "have", result)
 	}
